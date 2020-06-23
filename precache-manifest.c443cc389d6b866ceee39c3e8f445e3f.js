db6943eefe47e6a70f6ca82e738a226c82486417
rana-ghub <ssshashankrana001@gmail.com>
2020-06-24 03:22:11 +0530

    first commit

---
+++ precache-manifest.c443cc389d6b866ceee39c3e8f445e3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6376fbe8c6c3885c9de83932d1a692c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d49d3990f3d2c249d593",
+    "url": "/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "0fb6c7d8ecc540eb092e",
+    "url": "/static/js/2.cf52e512.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cf52e512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d49d3990f3d2c249d593",
+    "url": "/static/js/main.7f4f082d.chunk.js"
+  },
+  {
+    "revision": "afef85084f0804340619",
+    "url": "/static/js/runtime-main.2b370e64.js"
+  }
+]);
